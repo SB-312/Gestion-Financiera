5a5f9b24b0dfc846dfd115c9166904c1e855c7b3
SB-312 <116577635+SB-312@users.noreply.github.com>
2026-09-24 09:04:57 -0500

    Update Costos Fijos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='cf-v1',A=['./','index.html','manifest.json','icon-192.png','icon-512.png'];
+const C='cf-v2',A=['./','index.html','manifest.json','icon-192.png','icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(A)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
